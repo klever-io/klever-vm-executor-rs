3fc900f70afe76121709cb0832443c5bda7e278e
Yuri Lozorio <yurilozorio@gmail.com>
2024-05-21 15:50:37 -0300

    Added handle for kda admin address

--- vm-executor-wasmer/src/wasmer_imports.rs
+++ vm-executor-wasmer/src/wasmer_imports.rs
@@ -431,8 +431,8 @@ fn wasmer_import_managed_get_user_kda(env: &VMHooksWrapper, address_handle: i32,
 }
 
 #[rustfmt::skip]
-fn wasmer_import_managed_get_kda_token_data(env: &VMHooksWrapper, address_handle: i32, ticker_handle: i32, nonce: i64, precision_handle: i32, id_handle: i32, name_handle: i32, creator_handle: i32, logo_handle: i32, uris_handle: i32, initial_supply_handle: i32, circulating_supply_handle: i32, max_supply_handle: i32, minted_handle: i32, burned_handle: i32, royalties_handle: i32, properties_handle: i32, attributes_handle: i32, roles_handle: i32, issue_date_handle: i32) {
-    env.vm_hooks.managed_get_kda_token_data(address_handle, ticker_handle, nonce, precision_handle, id_handle, name_handle, creator_handle, logo_handle, uris_handle, initial_supply_handle, circulating_supply_handle, max_supply_handle, minted_handle, burned_handle, royalties_handle, properties_handle, attributes_handle, roles_handle, issue_date_handle)
+fn wasmer_import_managed_get_kda_token_data(env: &VMHooksWrapper, address_handle: i32, ticker_handle: i32, nonce: i64, precision_handle: i32, id_handle: i32, name_handle: i32, creator_handle: i32, admin_handle: i32, logo_handle: i32, uris_handle: i32, initial_supply_handle: i32, circulating_supply_handle: i32, max_supply_handle: i32, minted_handle: i32, burned_handle: i32, royalties_handle: i32, properties_handle: i32, attributes_handle: i32, roles_handle: i32, issue_date_handle: i32) {
+    env.vm_hooks.managed_get_kda_token_data(address_handle, ticker_handle, nonce, precision_handle, id_handle, name_handle, creator_handle, admin_handle, logo_handle, uris_handle, initial_supply_handle, circulating_supply_handle, max_supply_handle, minted_handle, burned_handle, royalties_handle, properties_handle, attributes_handle, roles_handle, issue_date_handle)
 }
 
 #[rustfmt::skip]
